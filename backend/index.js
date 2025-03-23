3487dd2be6c71f005751f8a68832cdfefbf6694c
neverlast121 <neverlast121@gmail.com>
2025-03-23 13:58:52 +0500

    Updated

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const { Sequelize } = require("sequelize");
-require("dotenv").config();
-
+require("dotenv").config({ path: path.join(__dirname, ".env") });
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
